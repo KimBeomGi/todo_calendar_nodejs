f99097260f12fbc6cdd0682d3db58a484877658f
김범기 <dyd1308@naver.com>
2024-07-03 22:35:03 +0900

    "날짜 선택 모달창 띄우는 로직"

--- todo_calendar/public/src/todorecord.js
+++ todo_calendar/public/src/todorecord.js
@@ -1,15 +1,46 @@
-let isClick_sel_sdate = false
+let isViewDate1 = false
+let isViewDate2 = false
 
+// 
 const sel_sdate_div = document.querySelector('#sel_sdate_div')
-const cs_dateDiv = document.querySelector('#cs_dateDiv')
-cs_dateDiv.classList.add('visibility_hidden')
+const sel_edate_div = document.querySelector('#sel_edate_div')
 
-sel_sdate_div.addEventListener('click', function(){
-  if(cs_dateDiv.classList.contains('visibility_hidden')){
-    cs_dateDiv.classList.remove('visibility_hidden');
-    isClick_sel_sdate = !isClick_sel_sdate
+const cs_dateDiv = document.querySelector('#cs_dateDiv')
+const ce_dateDiv = document.querySelector('#ce_dateDiv')
+
+// 초기에는 안보이게 하기
+cs_dateDiv.classList.add('visibility_hidden')
+ce_dateDiv.classList.add('visibility_hidden')
+
+// 껐다켰다
+function toggleVisibility(element, isVisible) {
+  if(isVisible){
+    element.classList.remove('visibility_hidden')
   }else{
-    cs_dateDiv.classList.add('visibility_hidden');
-    isClick_sel_sdate = !isClick_sel_sdate
-  };
+    element.classList.add('visibility_hidden')
+  }
+}
+
+function handleStateChange() {
+  toggleVisibility(cs_dateDiv, isViewDate1);
+  toggleVisibility(ce_dateDiv, isViewDate2);
+}
+
+sel_sdate_div.addEventListener('click', () => {
+  isViewDate1 = !isViewDate1
+  handleStateChange()
+})
+sel_edate_div.addEventListener('click', () => {
+  isViewDate2 = !isViewDate2
+  handleStateChange()
+})
+
+document.addEventListener('click', (event) => {
+  if (!sel_sdate_div.contains(event.target) && !cs_dateDiv.contains(event.target)) {
+      isViewDate1 = false;
+  }
+  if (!sel_edate_div.contains(event.target) && !ce_dateDiv.contains(event.target)) {
+      isViewDate2 = false;
+  }
+  handleStateChange();
 });
